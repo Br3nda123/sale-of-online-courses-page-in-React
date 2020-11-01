615c5e9505cecc886b8f0bb1283165adbf57d28e
Brandon Kunicki <brandon.kunicki@gmail.com>
2020-11-01 15:24:22 +0100

    feat: Navigation of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
 
-import StoreProvider from './store/StoreProvider';
+import AsideMenu from './components/AsideMenu/AsideMenu';
 import Header from './components/Header';
+import StoreProvider from './store/StoreProvider';
 
 import './App.scss';
 
 const App = () => {
   return ( 
     <StoreProvider>
-      <Header/>    
+      <Header />
+      <Router>
+        <div className="content-wrapper">
+          <AsideMenu/>
+        </div>
+      </Router>
     </StoreProvider>
    );
 }
